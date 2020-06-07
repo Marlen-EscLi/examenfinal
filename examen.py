85fc2838c6671d15185241a98e302caeb51474e4
Marlen <marlenescobarlima11@gmail.com>
2020-06-06 20:18:18 -0600

    Se hicieron modificaciones

--- examen.py
+++ examen.py
@@ -27,23 +27,23 @@ etiqueta.pack()
 #textetiqueta5 = Entry(ventana, textvariable = entrada).place(x = 180, y = 160)
 #Campo de texto y labels
 
-entrada1 = StringVar()
+self.entrada1 = StringVar()
 etiqueta1 = tkinter.Label(ventana, text = "Nombre", font = ("Century Gothic", 18)).place(x = 75, y = 40)
 textetiqueta1 = Entry(ventana, textvariable = entrada1, width = 30).place(x = 185, y = 43)
 
-entrada2 = StringVar()
+self.entrada2 = StringVar()
 etiqueta2 = tkinter.Label(ventana, text = "Apellido", font = ("Century Gothic", 18)).place(x = 75, y = 67)
 textetiqueta2 = Entry(ventana, textvariable = entrada2, width = 30).place(x = 185, y = 73)
 
-entrada3 = StringVar()
+self.entrada3 = StringVar()
 etiqueta3 = tkinter.Label(ventana, text = "Día", font = ("Century Gothic", 18)).place(x = 75, y = 95)
 textetiqueta3 = Entry(ventana, textvariable = entrada3, width = 30).place(x = 185, y = 100)
 
-entrada4 = StringVar()
+self.entrada4 = StringVar()
 etiqueta4 = tkinter.Label(ventana, text = "Mes", font = ("Century Gothic", 18)).place(x = 75, y = 125)
 textetiqueta4 = Entry(ventana, textvariable = entrada4, width = 30).place(x = 185, y = 130)
 
-entrada5 = StringVar()
+self.entrada5 = StringVar()
 etiqueta5 = tkinter.Label(ventana, text = "Año", font = ("Century Gothic", 18)).place(x = 75, y = 155)
 textetiqueta5 = Entry(ventana, textvariable = entrada5, width = 30).place(x = 185, y = 160)
 
@@ -56,8 +56,11 @@ botonf5 = Button(ventana, text = "Función 5", command = self.fn5, font = ("Cent
 
 #label de respuestas
 
+self.message = tkinter.Label(ventana, text = ' Resultdo ', fg = 'withe').place(x = 225, y = 140)
 
- self.message = Label(text = '', fg = 'white').place(x = 250, y = 220)
+    
+
+ 
  
   #creación de variables  
  def fn1(self):
